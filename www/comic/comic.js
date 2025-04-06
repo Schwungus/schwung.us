dd8f9bc1674f658237faf013c9a018666d0b4ea8
nonk123 <me@nonk.dev>
2025-04-06 19:02:50 +0300

    Bruh

--- www/comic/comic.js
+++ www/comic/comic.js
@@ -73,8 +73,8 @@ function setPage(page, force) {
     }
 
     params.set("page", page);
+
     window.history.pushState(null, "", "/comic?" + params.toString());
-    pageImg.src = "";
     pageImg.src = `${pagesUrl}${id}/${page}.png`;
 
     if (page <= 0) {
