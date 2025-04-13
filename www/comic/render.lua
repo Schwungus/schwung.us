fb9863468bd4612c3a3e5843372422259a5471ef
nonk123 <me@nonk.dev>
2025-04-13 10:18:47 +0300

    YOU FORGOT THE LUA PART!!!

--- www/comic/render.lua
+++ www/comic/render.lua
@@ -1,13 +1,11 @@
 local db = json("comic/db.json");
 
 for id, comic in pairs(db) do
-    if id ~= "" then
-        local outpath = "comic/" .. id .. "/index.html";
+    local outpath = "comic/" .. id .. "/index.html";
 
-        render("_comic.html", outpath, {
-            id = id,
-            title = comic.name,
-            description = comic.description,
-        });
-    end
+    render("_comic.html", outpath, {
+        id = id,
+        title = comic.name,
+        description = comic.description,
+    });
 end
